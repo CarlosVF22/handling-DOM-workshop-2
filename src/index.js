ba045ba04548b91e11b8187e4b7332225e40801e
Carlos Vasquez <cevasquezf22@gmail.com>
2021-10-07 17:44:49 -0500

    First commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,37 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import { registerImage } from "./lazy";
 
-console.log('Happy hacking :)')
+//crear 1 imagen
+//agregar al contenedor imagen
+const max = 123;
+const min = 1;
+const random =() => Math.floor(Math.random()*(max-min)) +min;
+
+const createImageNode = () =>{
+    //creando el contenedor de la imagen
+    const container = document.createElement("div");
+    container.className="p-4";
+
+    // creando la etiqueta img, dandole diseños y colocando la imagen por uri
+    const imagen =document.createElement('img');
+    imagen.className ="mx-auto";
+    imagen.width ="320";
+    imagen.src=`https://randomfox.ca/images/${random()}.jpg`; //ToDo (por hacer)
+
+    container.appendChild(imagen);
+
+    return container;
+};
+
+const mountNode =document.getElementById("images");
+
+const addButton = document.querySelector("button");
+
+const addImage = () => {
+    const newImage = createImageNode();
+    mountNode.append(newImage);
+    registerImage(newImage);
+};
+
+
+addButton.addEventListener("click",addImage);
+
