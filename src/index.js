389548f7c451f48859ed216bd94214d835729ad3
Carlos Vasquez <cevasquezf22@gmail.com>
2021-10-15 09:16:34 -0500

    agregando hyperscript

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import h from "hyperscript";
 import { registerImage } from "./lazy";
 
 //crear 1 imagen
@@ -8,14 +9,23 @@ const random =() => Math.floor(Math.random()*(max-min)) +min;
 
 const createImageNode = () =>{
     //creando el contenedor de la imagen
-    const container = document.createElement("div");
-    container.className="p-4";
+    // const container = document.createElement("div");
+    // container.className="p-4";
 
     // creando la etiqueta img, dandole diseños y colocando la imagen por uri
-    const imagen =document.createElement('img');
-    imagen.className ="mx-auto";
-    imagen.width ="320";
-    imagen.dataset.src=`https://randomfox.ca/images/${random()}.jpg`;
+    // const imagen =document.createElement('img');
+    // imagen.className ="mx-auto";
+    // imagen.width ="320";
+    // imagen.dataset.src=`https://randomfox.ca/images/${random()}.jpg`;
+
+    //utilizando hyperscript
+    const imagen = h('img.mx-auto',{ //paso atributos con los cual voy a inicializar
+        width:"320",
+        "data-src": `https://randomfox.ca/images/${random()}.jpg`,
+    });
+
+    //utilizando hyperscript
+    const container = h('div.p-4.mt-3',imagen); //el segundo parametro indica el hijo que se va a agregar al container
 
     //creando imgWrapper
     const imgWrapper = document.createElement("div");
@@ -23,7 +33,7 @@ const createImageNode = () =>{
     // imgWrapper.style.minHeight ="100px";
     imgWrapper.style.display ="inline-block";
 
-    imgWrapper.appendChild(imagen)
+    // imgWrapper.appendChild(imagen)
     container.appendChild(imgWrapper);
 
     appendedImages++;
